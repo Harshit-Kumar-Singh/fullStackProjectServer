1c9aee6202337510c5f1525b22b88716435190a2
Harshit-Kumar-Singh <kaumarharshit@gmail.com>
2021-11-09 23:33:38 +0530

    Third

---
+++ index2.js
@@ -0,0 +1,13 @@
+const http = require("http");
+
+const PORT = 5000;
+
+let x = http.createServer((req, res) => {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.end('billu')
+});
+
+x.listen(PORT, "0.0.0.0", () => {
+  console.log("Server Started");
+});
+
