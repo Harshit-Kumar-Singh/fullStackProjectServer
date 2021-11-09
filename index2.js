613f841fb4313ef7c37ab5d950c85f47c315a28d
Harshit-Kumar-Singh <kaumarharshit@gmail.com>
2021-11-09 23:37:09 +0530

    leleleelel

--- index2.js
+++ index2.js
@@ -1,13 +1,12 @@
 const http = require("http");
 
-const PORT = 5000;
+const PORT = process.env.PORT || 5000;
 
 let x = http.createServer((req, res) => {
   res.setHeader("Access-Control-Allow-Origin", "*");
-  res.end('billu')
+  res.end("billu");
 });
 
 x.listen(PORT, "0.0.0.0", () => {
   console.log("Server Started");
 });
-
